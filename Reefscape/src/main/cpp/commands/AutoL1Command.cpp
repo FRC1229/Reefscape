9d24e4e1af42bf2f518dd33b06b9f93b6a9921ec
KrishPatel <krishpatelsuper@gmail.com>
2025-02-25 13:18:32 -0500

    Update AutoL1Command.cpp
    Co-Authored-By: Yaksh Butani <145413456+YakshB07@users.noreply.github.com>

--- Reefscape/src/main/cpp/commands/AutoL1Command.cpp
+++ Reefscape/src/main/cpp/commands/AutoL1Command.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+
 #include "commands/AutoL1Command.h"
 
 AutoL1Command::AutoL1Command(L1Subsystem* l1, double angle ) :  m_l1(l1), m_angle(angle)  {
