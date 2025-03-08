594cdaf0125f988ab24566cbc98fdee2b0ecd671
medgarm <medgar_m@yahoo.com>
2025-03-08 12:52:11 -0500

    putting in some improvements
    putting in some improvements

--- Reefscape/src/main/cpp/subsystems/AlgaeSubsystem.cpp
+++ Reefscape/src/main/cpp/subsystems/AlgaeSubsystem.cpp
@@ -3,29 +3,34 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/AlgaeSubsystem.h"
+#include <frc/controller/PIDController.h>
 
 AlgaeSubsystem::AlgaeSubsystem():
 m_AlgaeTiltMotor(23,rev::spark::SparkMax::MotorType::kBrushless),
-m_AlgaeTiltEncoder(m_AlgaeTiltMotor.GetEncoder())
+m_AlgaeTiltEncoder(m_AlgaeTiltMotor.GetEncoder()),
+m_AlgaeController(0.05, 0.0, 0.0) // PID constants: P=0.05, I=0.0, D=0.0
 {
-    m_AlgaeTiltMotor.SetInverted(true);
+    m_AlgaeTiltMotor.SetInverted(true); // Inverting the motor if necessary
 }
 
 // This method will be called once per scheduler run
 void AlgaeSubsystem::Periodic() {}
 
 
-
+// Placeholder for manual tilt, not modified
 void AlgaeSubsystem::ManualTilt(){
 
 }
 
+// Get the current angle from the encoder
 double AlgaeSubsystem::GetAngle(){
     return m_AlgaeTiltEncoder.GetPosition();
 }
 
-void AlgaeSubsystem::MoveToAngle(double angle){
-    double volt = m_AlgaeController.Calculate(GetAngle(), angle);
-    m_AlgaeTiltMotor.SetVoltage(units::volt_t{volt});
-
+// Move to a specific angle using PID
+void AlgaeSubsystem::MoveToAngle(double angle) {
+    // Calculate the PID output
+    double pidOutput = m_AlgaeController.Calculate(GetAngle(), angle);
+    // Apply the PID output as voltage to the motor
+    m_AlgaeTiltMotor.SetVoltage(units::volt_t{pidOutput});
 }
--- Reefscape/src/main/cpp/subsystems/CoralSubsystem.cpp
+++ Reefscape/src/main/cpp/subsystems/CoralSubsystem.cpp
@@ -3,10 +3,12 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/CoralSubsystem.h"
+#include <frc/controller/PIDController.h>
 
 CoralSubsystem::CoralSubsystem(): 
 m_CoralTilt(24,rev::spark::SparkMax::MotorType::kBrushless), 
-m_CoralEncoder(m_CoralTilt.GetEncoder())
+m_CoralEncoder(m_CoralTilt.GetEncoder()),
+m_coralController(0.05, 0.0, 0.0) // PID constants: P=0.05, I=0.0, D=0.0
 {
     m_CoralTilt.SetInverted(true);
 }
@@ -18,7 +20,10 @@ double CoralSubsystem::GetAngle(){
     return m_CoralEncoder.GetPosition();
 }
 
-void CoralSubsystem::MoveToAngle(double angle){
-    double volt = m_coralController.Calculate(GetAngle(), angle);
-    m_CoralTilt.SetVoltage(units::volt_t{volt});
+// Move to a specific angle using PID
+void CoralSubsystem::MoveToAngle(double angle) {
+    // Calculate the PID output based on the target angle
+    double pidOutput = m_coralController.Calculate(GetAngle(), angle);
+    // Apply the PID output as voltage to the motor
+    m_CoralTilt.SetVoltage(units::volt_t{pidOutput});
 }
